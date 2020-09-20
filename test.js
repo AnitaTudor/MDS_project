73d7ec5f93ba7a853b4f5b7572bd196ef8badf7d
Anita Tudor <tudoranita1054@gmail.com>
2020-09-20 17:59:17 +0300

    Revert "Merge branch 'dev' of https://github.com/AnitaTudor/MDS_project into dev"
    This reverts commit 632e7e17c2238165119d929aa4574b3947ec52d2, reversing
    changes made to f6bfa2559b5a8010451ebb549cfedee5eb65f718.

--- test.js
+++
@@ -1,9 +0,0 @@
-var expect = require('chai').expect;
-var request = require('request');
-it('index main page', function(done) {
-
-    request('http://localhost:1111', function(error, response, body) {
-        expect(response.statuscode).to.equal(200);
-        done();
-    }).catch(done);
-});
